63777c68f8c5e94c986172532c0aaf860bf3c628
KorolER <elizavetakorol2005@gmail.com>
2024-09-20 23:39:59 +0300

    Laba1: finished

--- Laba1/app/src/main/java/com/example/laba1/MainActivity.kt
+++ Laba1/app/src/main/java/com/example/laba1/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,13 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -74,17 +70,17 @@ fun Greeting(modifier: Modifier = Modifier) {
                 Text(text = "X")
             }
         }
-        Row (modifier = modifier
-            .align(Alignment.End)){
-            Image(
-                painter = painterResource(id = R.drawable.cat),
-                contentDescription = "Cat",
-                modifier = modifier
-                    .align(Alignment.Bottom)
-                    .padding(16.dp)
-            )
-
-        }
+    }
+    Column (modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Bottom){
+        Image(
+            painter = painterResource(id = R.drawable.cat),
+            contentDescription = "Cat",
+            modifier = modifier
+                .padding(horizontal =  4.dp)
+                .padding(vertical = 4.dp)
+        )
     }
 }
 
