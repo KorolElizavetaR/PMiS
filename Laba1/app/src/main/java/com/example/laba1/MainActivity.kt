b97edaf1eb5fa80f98a57b32de219f45a876abbe
KorolER <elizavetakorol2005@gmail.com>
2024-09-20 23:30:15 +0300

    laba1

---
+++ Laba1/app/src/main/java/com/example/laba1/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.laba1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.laba1.ui.theme.Laba1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laba1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Column (modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        var TextObject by rememberSaveable  { mutableStateOf("") }
+        Text(
+            text = TextObject,
+            modifier = modifier
+        )
+        Row {
+            Button(
+                onClick =
+                {
+                    TextObject = "Karol Liza"
+                }
+            ) {
+                Text(text = "My name")
+            }
+            Button(
+                onClick =
+                {
+                    TextObject = ""
+                }
+            ) {
+                Text(text = "X")
+            }
+        }
+        Row (modifier = modifier
+            .align(Alignment.End)){
+            Image(
+                painter = painterResource(id = R.drawable.cat),
+                contentDescription = "Cat",
+                modifier = modifier
+                    .align(Alignment.Bottom)
+                    .padding(16.dp)
+            )
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laba1Theme {
+        Greeting()
+    }
+}
