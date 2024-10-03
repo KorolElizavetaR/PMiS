c2246710437a61e116429180b52560b3a1a2cfec
KorolER <elizavetakorol2005@gmail.com>
2024-10-03 11:24:43 +0300

    бубун

--- Laba2Start/app/src/main/java/com/start/laba2start/MainActivity.kt
+++ Laba2Start/app/src/main/java/com/start/laba2start/MainActivity.kt
@@ -3,28 +3,23 @@ package com.start.laba2start
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.compose.composable
-import com.start.laba2start.ui.theme.Laba2StartTheme
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 
 class MainActivity : ComponentActivity() {
---
+++ Laba2Task/app/src/main/java/com/example/laba1/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.laba1
+
+
+import android.content.res.Resources
+import android.os.Bundle
+import android.provider.Settings.Secure.getString
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.laba1.ui.theme.Laba1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laba1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Column (modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        val MyName = stringResource(id = R.string.myName)
+        val emptyValue = stringResource(id = R.string.emptyVal)
+
+        var TextObject by rememberSaveable  { mutableStateOf("") }
+        Text(
+            text = TextObject,
+            modifier = modifier
+        )
+        Row {
+            Button(
+                onClick =
+                {
+                    TextObject = "i am $MyName"
+                }
+            ) {
+                Text(text = stringResource(id = R.string.mynameButton))
+            }
+            Button(
+                onClick =
+                {
+                    TextObject = emptyValue
+                }
+            ) {
+                Text(text = stringResource(id = R.string.XButton))
+            }
+        }
+    }
+    Column (modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Bottom){
+        Image(
+            painter = painterResource(id = R.drawable.cat),
+            contentDescription = "Cat",
+            modifier = modifier
+                .padding(horizontal = 4.dp)
+                .padding(vertical = 4.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laba1Theme {
+        Greeting()
+    }
+}
