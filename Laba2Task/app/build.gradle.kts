5a75181be44ddb0617e6169273d6fa0391c8cc23
KorolER <elizavetakorol2005@gmail.com>
2024-10-03 11:42:19 +0300

    оно, кажись, работает

--- Laba2Task/app/build.gradle.kts
+++ Laba2Task/app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
 }
 
 dependencies {
-
+    val nav_version = "2.8.2"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- Laba2Task/app/src/main/java/com/example/laba1/MainActivity.kt
+++ Laba2Task/app/src/main/java/com/example/laba1/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.laba1
 
 
-import android.content.res.Resources
 import android.os.Bundle
-import android.provider.Settings.Secure.getString
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,20 +11,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.laba1.ui.theme.Laba1Theme
 
 class MainActivity : ComponentActivity() {
@@ -35,18 +45,80 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Laba1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Greeting()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun Greeting() {
+    val navController = rememberNavController()
+    Column(Modifier.padding(8.dp)) {
+        NavHost(
+            navController, startDestination = NavRoutes.Home.route, modifier
+            = Modifier.weight(1f)
+        ) {
+            composable(NavRoutes.Home.route) { Home(modifier = Modifier.padding(8.dp)) }
+            composable(NavRoutes.About.route) { About() }
+        }
+        BottomNavigationBar(navController = navController)
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    NavigationBar {
+        val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = backStackEntry?.destination?.route
+        NavBarItems.BarItems.forEach { navItem ->
+            NavigationBarItem(
+                selected = currentRoute == navItem.route,
+                onClick = {
+                    navController.navigate(navItem.route) {
+                        popUpTo(navController.graph.findStartDestination().id)
+                        { saveState = true }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = {
+                    Icon(
+                        imageVector = navItem.image,
+                        contentDescription = navItem.title
+                    )
+                },
+                label = {
+                    Text(text = navItem.title)
+                }
+            )
+        }
+    }
+}
+
+object NavBarItems {
+    val BarItems = listOf(
+        BarItem(
+            title = "Главная",
+            image = Icons.Filled.Home,
+            route = "home"
+        ),
+        BarItem(
+            title = "Что тут?",
+            image = Icons.Filled.Info,
+            route = "about"
+        )
+    )
+}
+
+data class BarItem(
+    val title: String,
+    val image: ImageVector,
+    val route: String
+)
+
+@Composable
+fun Home(modifier: Modifier = Modifier) {
     Column (modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center)
@@ -91,10 +163,12 @@ fun Greeting(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Laba1Theme {
-        Greeting()
-    }
+fun About() {
+    Text("About Page", fontSize = 30.sp)
+}
+
+sealed class NavRoutes(val route: String) {
+    object Home : NavRoutes("home")
+    object About : NavRoutes("about")
 }
