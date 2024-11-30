75dab8a463c9d443881bbeb064adb22d24f25310
KorolER <elizavetakorol2005@gmail.com>
2024-11-30 10:57:04 +0300

    последний коммит

--- Catalog/app/src/main/java/com/catalog/ui_components/MainScreen.kt
+++ Catalog/app/src/main/java/com/catalog/ui_components/MainScreen.kt
@@ -1,8 +1,6 @@
 package com.catalog.ui_components
 
 import android.annotation.SuppressLint
-import android.os.Build
-import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalDrawerSheet
@@ -15,36 +13,91 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.catalog.MainViewModel
 import com.catalog.utils.DrawerEvents
 import com.catalog.utils.ListItem
 import kotlinx.coroutines.launch
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@RequiresExtension(extension = Build.VERSION_CODES.S, version = 13)
+//@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+//@RequiresExtension(extension = Build.VERSION_CODES.S, version = 13)
+//@Composable
+//fun MainScreen(
+//    mainViewModel: MainViewModel = hiltViewModel(), onClick: (ListItem) -> Unit
+//) {
+//    val topBarTitle = rememberSaveable { mutableStateOf("Бизнес Литература") }
+//    val drawerState = rememberDrawerState(DrawerValue.Closed)
+//    val scope = rememberCoroutineScope()
+//    val mainlist = mainViewModel.mainList
+//
+//    LaunchedEffect(Unit) {
+//        mainViewModel.getAllItemsByCategory(topBarTitle.value)
+//    }
+//
+//    ModalNavigationDrawer(
+//        drawerState = drawerState,
+//        drawerContent = {
+//            ModalDrawerSheet(drawerShape = RectangleShape) {
+//                DrawerMenu() { event ->
+//                    when (event) {
+//                        is DrawerEvents.OnItemClick -> {
+//                            topBarTitle.value = event.title
+//                            mainViewModel.getAllItemsByCategory(event.title)
+//                        }
+//                    }
+//                    scope.launch {
+//                        drawerState.close()
+//                    }
+//                }
+//            }
+//        },
+//        content = {
+//            Scaffold(
+//                topBar = {
+//                    MainTopBar(title = topBarTitle.value, drawerState = drawerState){
+//                        topBarTitle.value = "Избранные"
+//                        mainViewModel.getFavorites()
+//                    }
+//                }
+//            ) { innerPadding ->
+//                MainListItemRows(
+//                    items = mainlist.value,
+//                    modifier = Modifier.padding(innerPadding),
+//                ) { listItem -> onClick(listItem) }
+//            }
+//        }
+//
+//    )
+//}
+
 @Composable
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 fun MainScreen(
-    mainViewModel: MainViewModel = hiltViewModel(), onClick: (ListItem) -> Unit
+    mainViewModel: MainViewModel = hiltViewModel(),
+    onClick: (ListItem)->Unit
 ) {
     val topBarTitle = rememberSaveable { mutableStateOf("Бизнес Литература") }
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
-    val mainlist = mainViewModel.mainList
 
-    LaunchedEffect(Unit) {
-        mainViewModel.getAllItemsByCategory(topBarTitle.value)
+    val mainList = mainViewModel.mainList
+
+    LaunchedEffect(topBarTitle.value) {
+        if (topBarTitle.value == "Избранное") {
+            mainViewModel.getFavorites()
+        } else {
+            mainViewModel.getAllItemsByCategory(topBarTitle.value)
+        }
     }
 
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet(drawerShape = RectangleShape) {
-                DrawerMenu() { event ->
-                    when (event) {
+            ModalDrawerSheet {
+                DrawerMenu { event ->
+                    when(event) {
                         is DrawerEvents.OnItemClick -> {
-                            topBarTitle.value = event.title
+                            topBarTitle.value=event.title
                             mainViewModel.getAllItemsByCategory(event.title)
                         }
                     }
@@ -57,18 +110,18 @@ fun MainScreen(
         content = {
             Scaffold(
                 topBar = {
-                    MainTopBar(title = topBarTitle.value, drawerState = drawerState){
-                        topBarTitle.value = "Избранные"
+                    MainTopBar(title = topBarTitle.value, drawerState = drawerState) {
+                        topBarTitle.value = "Избранное"
                         mainViewModel.getFavorites()
                     }
                 }
             ) { innerPadding ->
                 MainListItemRows(
-                    items = mainlist.value,
+                    items = mainList.value,
                     modifier = Modifier.padding(innerPadding),
                 ) { listItem -> onClick(listItem) }
             }
-        }
 
+        }
     )
 }
