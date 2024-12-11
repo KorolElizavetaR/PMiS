e6bab59a1687e2f93343e3d3214d07af18b4257a
KorolER <elizavetakorol2005@gmail.com>
2024-12-11 20:39:59 +0300

    init commit

---
+++ BookNotes/app/src/main/java/com/booknotes/db/Book.kt
@@ -0,0 +1,4 @@
+package com.booknotes.db
+
+class Book {
+}
--- Catalog/app/src/main/java/com/catalog/ui_components/InfoScreen.kt
+++ Catalog/app/src/main/java/com/catalog/ui_components/InfoScreen.kt
@@ -1,7 +1,9 @@
 package com.catalog.ui_components
 
+import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.graphics.Bitmap
+import android.os.Bundle
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -17,9 +19,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -28,6 +30,7 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.ViewModel
 import com.catalog.utils.ListItem
 
 @Composable
@@ -89,7 +92,7 @@ fun ContentSection(
                 .padding(bottom = 8.dp)
         )
     }
-    HtmlLoader(htmlName = htmlName)
+    HtmlLoader(htmlName = htmlName, modifier = Modifier.fillMaxWidth())
 }
 
 
@@ -100,49 +103,120 @@ fun ContentSection(
 //    if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
 
 
+//@Composable
+//fun HtmlLoader(htmlName: String) {
+//    var backEnabled by rememberSaveable { mutableStateOf(false) }
+//    var webView: WebView? = null
+//    val context = LocalContext.current
+//    val assetManger = context.assets
+//    val inputStream = assetManger.open("html/$htmlName")
+//    val size = inputStream.available()
+//    val buffer = ByteArray(size)
+//    inputStream.read(buffer)
+//    val htmlString = String(buffer)
+//    var prevPage: String? by rememberSaveable { mutableStateOf(null) }
+//
+//    AndroidView(modifier = Modifier
+//        .fillMaxSize()
+//        .padding(5.dp),
+//        factory = { context ->
+//            WebView(context).apply {
+//                layoutParams = ViewGroup.LayoutParams(
+//                    ViewGroup.LayoutParams.MATCH_PARENT,
+//                    ViewGroup.LayoutParams.MATCH_PARENT
+//                )
+//                webViewClient = object : WebViewClient() {
+//                    override fun onPageStarted(
+//                        view: WebView, url: String?,
+//                        favicon: Bitmap?
+//                    ) {
+//                        backEnabled = view.canGoBack()
+//                        prevPage = url
+//                    }
+//                }
+//                settings.javaScriptEnabled = true
+//
+//                if (prevPage == null)
+//                    loadData(htmlString, "text/html", "utf-8")
+//                else
+//                    loadUrl(prevPage!!)
+//                webView = this
+//            }
+//        }, update = {
+//            webView = it
+//        })
+//    BackHandler(enabled = backEnabled) {
+//        webView?.goBack()
+//    }
+//}
+
+@SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun HtmlLoader(htmlName: String) {
-    var backEnabled by remember { mutableStateOf(false) }
-    var webView: WebView? = null
+fun HtmlLoader(
+    htmlName: String,
+    modifier: Modifier,
+    viewModel: WebViewViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
+) {
+    var backEnabled by rememberSaveable { mutableStateOf(false) }
     val context = LocalContext.current
+
     val assetManger = context.assets
     val inputStream = assetManger.open("html/$htmlName")
     val size = inputStream.available()
     val buffer = ByteArray(size)
     inputStream.read(buffer)
     val htmlString = String(buffer)
-    var prevPage: String? by rememberSaveable { mutableStateOf(null) }
 
-    AndroidView(modifier = Modifier
-        .fillMaxSize()
-        .padding(5.dp),
-        factory = { context ->
+    var webView: WebView? = null
+
+    AndroidView(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(5.dp),
+        factory = {
             WebView(context).apply {
                 layoutParams = ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT
                 )
+                settings.javaScriptEnabled = true
+
                 webViewClient = object : WebViewClient() {
                     override fun onPageStarted(
-                        view: WebView, url: String?,
+                        view: WebView,
+                        url: String?,
                         favicon: Bitmap?
                     ) {
                         backEnabled = view.canGoBack()
-                        prevPage = url
                     }
                 }
-                settings.javaScriptEnabled = true
 
-                if (prevPage == null)
-                    loadData(htmlString, "text/html", "utf-8")
-                else
-                    loadUrl(prevPage!!)
+                if (viewModel.webViewState.isEmpty) {
+                    loadDataWithBaseURL(null, htmlString, "text/html", "utf-8", null)
+                } else {
+                    restoreState(viewModel.webViewState)
+                }
+
                 webView = this
             }
-        }, update = {
-            webView = it
-        })
+        },
+        update = { view ->
+            webView = view
+            backEnabled = view.canGoBack()
+        }
+    )
+
+    DisposableEffect(Unit) {
+        onDispose {
+            webView?.saveState(viewModel.webViewState)
+        }
+    }
+
     BackHandler(enabled = backEnabled) {
         webView?.goBack()
     }
+}
+
+class WebViewViewModel : ViewModel() {
+    val webViewState = Bundle()
 }
